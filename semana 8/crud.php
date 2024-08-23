65f7d0473abdc4891c717c9d8da9572e2ea70ec3
Nathan Monteiro <contato.mont@gmail.com>
2024-08-22 23:25:00 -0300

    Primeira parte da estilização. Adição de pastas separadas para as imagens (perfil, background, etc...) Remoção de espaços em branco dentro do código (crud.php) Tradução das frases em inglês para português (email is required = e-mail é necessário, ...) Adição de uma nova CSS

--- semana 8/crud.php
+++ semana 8/crud.php
@@ -1,33 +1,26 @@
-<?php 
-
+<?php
     include "connect.inc.php";
     include "estudante.class.php";
 
     $matricula = 0;
     $action = 'insert';
-    $actionVal = 'CADASTRAR';
+    $actionVal = 'Cadastrar';
 
     if (!empty($_GET['action'])) {
         
         $action = $_GET['action'];
-        $actionVal = 'ATUALIZAR';
+        $actionVal = 'Atualizar';
         $matricula = $_GET['matricula'];
     }
 
     $estudante = new Estudante($conn);
 
     $res = $estudante->read();
-    
-
-
     $resOne = $estudante->readOne('-1');
-
-    
 
     if($action =='update'){
         $resOne = $estudante->readOne($matricula);
     }
-    
 ?>
 
 
@@ -43,12 +36,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
     $EstaValido = True;
 
   if (empty($_POST["nome"])) {
-    $nameErr = "Name is required";
+    $nameErr = "Adicione um nome!";
     $EstaValido = False;
   } else {
     $nome = test_input($_POST["nome"]);
     if (!preg_match("/^[a-zA-Z-' ]*$/",$nome)) {
-        $nameErr = "Only letters and white space allowed";
+        $nameErr = "Apenas letras e espaços são permitidos";
         $valName = "ERRADO";
         $EstaValido = False;
     } else {
@@ -57,39 +50,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
   }
   
   if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
+    $emailErr = "Um e-mail é necessário!";
     $EstaValido = False;
   } else {
     $email = test_input($_POST["email"]);
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $emailErr = "Invalid email format";
+        $emailErr = "Formato de e-mail inválido!";
         $valEmail = "ERRADO";
         $EstaValido = False;
     } else {
         $valEmail = "CERTO";
     }
   }
-
+  
   if (empty($_POST["senha"])) {
     $senha = "";
     $EstaValido = False;
-    $senErr = "Senha requerida";
+    $senErr = "Senha necessária!";
   } else {
     $senha = test_input($_POST["senha"]);
-    
-        $valsenha = "CERTO";
-    
+    $valsenha = "CERTO";
   }
 
   if (empty($_POST["matricula"])) {
     $matricula = "";
     $EstaValido = False;
-    $matriErr = "Matricula requerida";
+    $matriErr = "Matricula necessária!";
   } else {
     $matricula = test_input($_POST["matricula"]);
-    
     $valmat = "CERTO";
-    
   }
 }
 
@@ -102,7 +91,6 @@ function test_input($data) {
 }
 
 if ($EstaValido) {
-
     if (empty($_POST['action'])) {
         $action = $_GET['action'];
         $matricula = $_GET['matricula'];
@@ -116,48 +104,61 @@ if ($EstaValido) {
         $estudante->update($_POST);
     }
     
-
     header('Location: login.php');
   }
 ?>
 
+
 <html>
     <head>
-        <title>Cadastro</title>
-        <link rel="stylesheet" href="./estilos/styles.css">
-        <style>.error {color: #FF0000;}</style>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./estilos/crudCad.css">
+        <title>CADASTRO</title>
     </head>
     <body>
         <!-- Conteúdo -->
-         <section class="container">
-            <div class="">
-                <form action="<?php echo $FormularioAcao; ?>" method="post">
-                    
-                    <input type="hidden" name="action" value="<?php echo $action; ?>">
+        <main>
+            <section class="container_general">
+                <div class="container_form">
+                    <form action="<?php echo $FormularioAcao; ?>" method="post">
+                        <input type="hidden" name="action" value="<?php echo $action; ?>">
 
-                    Name: <input type="text" name="nome" value="<?php echo !empty($resOne) ? $resOne[0]['nome'] : $nome; ?>">
-                    <span class="error">* <?php echo $nameErr;?></span>
-                    <br>
-                    E-mail: <input type="text" name="email" value="<?php echo !empty($resOne) ? $resOne[0]['email'] : $email; ?>">
-                    <span class="error">* <?php echo $emailErr;?></span>
-                    <br>
-                    Senha: <input type="text" name="senha" value="<?php echo !empty($resOne) ? $resOne[0]['senha'] : $senha; ?>">
-                    
-                    <br>
-                    Matricula: <input type="text" name="matricula" value="<?php echo !empty($resOne) ? $resOne[0]['matricula'] : $matricula; ?>">
-                    
-                    <br>
-                    <input type="submit" name="submit" value="<?php echo $actionVal ?>">
-                    <a href="login.php" class="btn btn-secondary">Login</a>  
-                    <input type="hidden" name="valMat" value="">
-                    <input type="hidden" name="valName" value="<?php echo $valName; ?>">
-                    <input type="hidden" name="valEmail" value="<?php echo $valEmail; ?>">
-                    <input type="hidden" name="valSenha" value="">
-                </form>
-            </div>            
-         </section>  
+                        <label for="name">Nome: </label>
+                        <input type="text" name="nome" id="name" placeholder="Digite seu nome..." value="<?php echo !empty($resOne) ? $resOne[0]['nome'] : $nome; ?>">
+                        <span class="error">* <?php echo $nameErr;?></span>
+
+                        <!-- <br> -->
+                        <label for="email">Email: </label>
+                        <input type="text" name="email" id="email" placeholder="Digite seu e-mail..." value="<?php echo !empty($resOne) ? $resOne[0]['email'] : $email; ?>">
+                        <span class="error">* <?php echo $emailErr;?></span>
+
+                        <!-- <br> -->
+                        <label for="password">Senha: </label>
+                        <input type="text" name="senha" id="password" placeholder="Digite uma senha..." value="<?php echo !empty($resOne) ? $resOne[0]['senha'] : $senha; ?>">
+
+                        <!-- <br> -->
+                        <label for="matricula">Matricula: </label>
+                        <input type="text" name="matricula" id="matricula" placeholder="Digite sua matrícula..." value="<?php echo !empty($resOne) ? $resOne[0]['matricula'] : $matricula; ?>">
+
+                        <!-- <br> -->
+
+                        <input type="submit" name="submit" value="<?php echo $actionVal ?>">
+
+                        <p class="login_text">Já possui uma conta?<a href="login.php" class="btn btn-secondary">Faça seu login</a></p>
+
+                        <input type="hidden" name="valMat" value="">
+                        <input type="hidden" name="valName" value="<?php echo $valName; ?>">
+                        <input type="hidden" name="valEmail" value="<?php echo $valEmail; ?>">
+                        <input type="hidden" name="valSenha" value="">
+                    </form>
+                </div>
+            </section>
+
+            <section class="bg_grid"></section>
+        </main>
         <!-- Lista -->
-        <section class="container">
+        <!-- <section class="container">
             <table>
                 <tr>
                     <th>matricula</th>
@@ -183,7 +184,7 @@ if ($EstaValido) {
     }
 ?>
             </table>
-        </section>
+        </section> -->
     </body>
 
 </html>
