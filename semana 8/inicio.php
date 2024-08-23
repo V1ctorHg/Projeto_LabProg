9ecc156679c2abea0bdbfb05d64d3e8a86089f4b
Nathan Monteiro <contato.mont@gmail.com>
2024-08-23 18:16:37 -0300

    Estilização parte 1. Estilizada a parte do menu na área do aluno. Ainda falta as alterações na parte da direita.

--- semana 8/inicio.php
+++ semana 8/inicio.php
@@ -1,3 +1,5 @@
+<!-- TELA DO ESTUDANTE -->
+
 <?php
     session_start();
     include "connect.inc.php";
@@ -35,50 +37,54 @@ $_SESSION['matricula'] = $matricula;
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./estilos/inicio.css">
-    <title>Página Inicial</title>
+    <title>INÍCIO — <?php echo $nome; ?></title>
 </head>
 <body>
-    <header class="container_top">
-        <nav>
-            <ul>
-                <li><h1>Bem-vindo, <?php echo $nome; ?>!</h1></li>
-                    <ul class="selections">
-                        <li><a class="about_link" href="#ranking">Ranking</a></li>
-                        <li class="vertical-row"></li>
-                        <li><a class="about_link" href="#">Editar</a></li>                        <li class="vertical-row"></li>
-                        <li><a class="about_link" href="login.php">Logout</a></li>
-                    </ul>
-                <li><strong>Matrícula:</strong> <?php echo $matricula; ?></a></li>
-            </ul>
-        </nav>
-    </header>
-    <section class="infos"> <!-- Impressão dos cursos | Falta realizar a inscrição com o botão -->
-    <?php if (!empty($res)): ?>
-        <table>
-            <tr>
-                <th>cod</th>
-                <th>nome</th>
-                <th>data</th>
-                <th>hora</th>
-            </tr>
-            <?php 
-            foreach ($res as $r) {
-                echo ("
+    <div class="container_general">
+        <header>
+            <div class="container_top_info">
+                <p class="text_info username">Bem vindo, <span class="bold"><?php echo $nome; ?></span>!</p>
+                <p class="text_info user_email">E-mail: <?php echo $email; ?></p>
+                <p class="text_info user_mat">Matrícula: <span class="bold"><?php echo $matricula; ?></span></p>
+            </div>
+        
+            <nav class="side_menu">
+                <ul class="menu_list">
+                    <li><a class="about_link" href="#ranking">Ranking</a></li>
+                    <li><a class="about_link" href="#">Editar</a></li>
+                    <li><a class="about_link" href="login.php">Logout</a></li>
+                </ul>
+            </nav>
+        </header>
+        <!-- Impressão dos cursos | Falta realizar a inscrição com o botão -->
+        <section class="infos">
+            <?php if (!empty($res)): ?>
+                <table>
                     <tr>
-                        <td>{$r['cod_curso']}</td>
-                        <td>{$r['nome']}</td>
-                        <td>{$r['datahora_ini']}</td>
-                        <td>{$r['horas']}</td>
-                    </tr>");
-            }
-            ?>
-        </table>
-    <?php else: ?>
-        <p>Sem cursos no momento.</p>
-    <?php endif; ?>
-    </section>  <!-- Fim da impressão -->
+                        <th>cod</th>
+                        <th>nome</th>
+                        <th>data</th>
+                        <th>hora</th>
+                    </tr>
+                    <?php
+                    foreach ($res as $r) {
+                        echo ("
+                            <tr>
+                                <td>{$r['cod_curso']}</td>
+                                <td>{$r['nome']}</td>
+                                <td>{$r['datahora_ini']}</td>
+                                <td>{$r['horas']}</td>
+                            </tr>");
+                    }
+                    ?>
+                </table>
+            <?php else: ?>
+                <p>Sem cursos no momento.</p>
+            <?php endif; ?>
+        </section>  <!-- Fim da impressão -->
+    </div>
 </body>
 </html>
