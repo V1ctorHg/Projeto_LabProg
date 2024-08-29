dff801abbdd2e703d0f593b6e0aa7ecf870c93da
Nathan Monteiro <contato.mont@gmail.com>
2024-08-29 19:56:23 -0300

    Estilização

--- semana 8/inicioOrganizador.php
+++ semana 8/inicioOrganizador.php
@@ -58,7 +58,7 @@ $eventos = [];
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./estilos/organizador2.css">
+    <link rel="stylesheet" href="./estilos/organizador.css">
     <title>INÍCIO — <?php echo $nome; ?></title>
 </head>
 <body>
